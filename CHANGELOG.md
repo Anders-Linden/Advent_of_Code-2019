f835dc1ea728185e6e6fb32fe302db24ce878655
Anders Lindén <anders@andvision.se>
2019-12-01 07:52:51 +0100

    New: Adding Day 1 solution

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+<a name="unreleased"></a>
+## [Unreleased]
+
+
+<a name="Day_01"></a>
+## Day_01 - 2019-12-01
+### New
+- Adding Day 1 solution
+
+
+[Unreleased]: https://gitlab.com/linden/advent_of_code-2018/compare/Day_01...HEAD
---
+++ src/day_01.rs
@@ -0,0 +1,63 @@
+use std::io::prelude::*;
+extern crate utilities as utils;
+
+fn calc_fuel(mass: i32) -> i32 {
+	(mass / 3 - 2)
+}
+
+fn calc_fuel_mass(mass: u32, sum: u32) -> u32 {
+	let result = calc_fuel(mass as i32);
+	if result > 0 {
+		calc_fuel_mass(result as u32, sum + result as u32)
+	} else {
+		sum
+	}
+}
+
+fn part1(data: &Vec<u32>) -> u32 {
+	let total_fuel: u32 = data
+		.into_iter()
+		.map(|x| calc_fuel(x.clone() as i32) as u32)
+		.sum();
+	return total_fuel;
+}
+
+fn part2(data: &Vec<u32>) -> u32 {
+	let total_fuel: u32 =
+		data.into_iter().map(|x| calc_fuel_mass(*x, 0) as u32).sum();
+
+	return total_fuel;
+}
+
+fn main() {
+	let mut data: Vec<u32> = Vec::new();
+	for line in utils::open_input("./assets/input_day01").lines() {
+		data.push(line.unwrap().parse().unwrap())
+	}
+
+	println!("Results for Day 1");
+	println!("-------------------");
+	println!("Part 1: {}", part1(&data));
+	println!("Part 2: {}", part2(&data));
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn code_coverage_test_main() {
+		assert_eq!(main(), ());
+	}
+
+	#[test]
+	fn fuel() {
+		assert_eq!(calc_fuel(1969), 654);
+		assert_eq!(calc_fuel(100756), 33583);
+	}
+	#[test]
+	fn fuel_mass() {
+		assert_eq!(calc_fuel_mass(1969, 0), 966);
+		assert_eq!(calc_fuel_mass(100756, 0), 50346);
+	}
+}
